54b34e5b8fbd9db95656c098c58c01c3a76ded69
William Mora <wilmor24@gmail.com>
2014-10-01 18:28:26 -0300

    Intro to commenting

--- Funky Unit Converter/Funky Unit Converter/ViewController.m
+++ Funky Unit Converter/Funky Unit Converter/ViewController.m
@@ -17,8 +17,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    // This is a comment
+    // This is another comment
+    
+    /*
+     
+     Information on this line
+     Additional info here
+     
+     */
+    // I am creating a variable named x that is of type integer. Integers are whole numbers
     int x;
+    // I am assigning the value of 5 to my variable x
     x = 5;
+    // My variable named heightOfEverestBaseCamp is being assigned the value 16900.3 since my variable is of type float. Floats are numbers with decimals
     float heightOfEverestBaseCamp = 16900.3;
 }
 
