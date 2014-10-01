87d6c93509267b460f0d62cb8df65fe33feeb1fb
William Mora <wilmor24@gmail.com>
2014-10-01 14:59:31 -0300

    Label use

---
+++ FirstApp/FirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstApp
+//
+//  Created by William Mora on 1/10/14.
+//  Copyright (c) 2014 William Mora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstApp/FirstAppTests/FirstAppTests.m
@@ -0,0 +1,40 @@
+//
+//  FirstAppTests.m
+//  FirstAppTests
+//
+//  Created by William Mora on 1/10/14.
+//  Copyright (c) 2014 William Mora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface FirstAppTests : XCTestCase
+
+@end
+
+@implementation FirstAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
